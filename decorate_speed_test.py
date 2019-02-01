4a6ab967f8d3fe47c4feb725af6906b33fe7c576
dKosarevsky <kosarevsky.d@ya.ru>
2019-02-01 06:45:49 +0300

    добавил декоратор с аргументами

--- decorate_speed_test.py
+++ decorate_speed_test.py
@@ -33,7 +33,7 @@ def generate_speed_test(n):
     return l
 
 
-l1 = iterate_speed_tests(10000)
+l1 = iterate_speed_tests(10**4)
 l2 = generate_speed_test(10**4)
 
 # print(l1)
@@ -48,3 +48,41 @@ l2 = generate_speed_test(10**4)
 # l1 = timeit(iterate_speed_tests)
 # # print(type(l1), l1.__name__)
 # a = l1(10)
+
+
+
+# Декоратор, который принимает аргументы:
+
+def timeit_a(arg):
+    print(arg)
+    def outer(func):
+        def wrapper(*args, **kwargs):
+            start = datetime.now()
+            result = func(*args, **kwargs)
+            print(datetime.now() - start)
+            return result
+        return wrapper
+    return outer
+
+
+
+@timeit_a('name')
+def iterate_speed_tests_a(n):
+    # start = datetime.now()
+    l = []
+    for i in range(n):
+        if i % 2 == 0:
+            l.append(i)
+    # print(datetime.now() - start)
+    return l
+
+
+@timeit_a('name')
+def generate_speed_test_a(n):
+    # start = datetime.now()
+    l = [x for x in range(n) if x % 2 == 0]
+    # print(datetime.now() - start)
+    return l
+
+iterate_speed_tests_a(10**4)
+generate_speed_test_a(10**4)
